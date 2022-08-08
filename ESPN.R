0d42058ba12d60a6a44fe00428fcd170cce3a53b
Nick <84089011+nflo90@users.noreply.github.com>
2022-08-08 16:35:41 -0500

    Update and rename ESPN.Rmd to ESPN.R

---
+++ ESPN.R
@@ -0,0 +1,258 @@
+library(tidyverse)
+library(purrr)
+library(rvest)
+library(janitor)
+
+
+#read base url}
+base.url <- read_html("https://www.espn.com/nfl/teams")
+
+#grab team names from base url
+teams <- base.url %>% 
+  html_nodes(".h5") %>% 
+  html_text()
+teams = teams[1:32]
+
+#grab urls to each team schedule
+url <- base.url %>% 
+  html_nodes(".nowrap:nth-child(2) .AnchorLink") %>% 
+  html_attr("href") %>% 
+  paste("https://www.espn.com", ., sep = "") 
+url = url[1:32]
+
+#bind teams and url
+team.links <- tibble(teams, url) %>% 
+  #select season to scrape schedule 
+  transmute(url = glue::glue("{url}/season/2021"))
+
+################################################################################
+#scrape schedules 
+results_list <- tibble(
+  html_results = map(team.links$url[1:32],
+                     ~ {
+                       Sys.sleep(2)
+                       # DO THIS!  sleep 2 will pause 2 seconds between server requests
+                       #to avoid being identified and potentially blocked /
+                       #assume crawling bot is a DNS attack.
+                       .x %>%
+                         read_html()
+                     }),
+  summary_url = team.links$url[1:32]
+)
+
+#get game IDs from schedules
+game.ids <- tibble(summary_url = results_list$summary_url, 
+                   team = 
+                     map(results_list$html_results,
+                         ~ .x %>%
+                           html_nodes("#fittPageContainer .db") %>%
+                           html_text()),
+                   game =
+                     map(results_list$html_results,
+                         ~ .x %>%
+                           html_nodes(".ml4 .AnchorLink") %>%
+                           html_attr("href")))
+
+################################################################################
+#disaggregate results and create df of game links 
+game.links <- game.ids %>% 
+  select(team, game) %>% 
+  pivot_wider(., names_from = team, values_from = game) %>% 
+  drop_na() %>% 
+  clean_names() %>% 
+  pivot_longer(cols = c("c_buffalo_bills":"c_seattle_seahawks")) %>% 
+  unnest(cols = everything())
+game.links$name <- sub("c_*", "", game.links$name)
+
+#single out ids + build new links to game data table, 
+#unique removes duplicates that were scraped twice due to away + home teams
+game.links$id = substr(game.links$value,40,49)
+game.links <- game.links %>% 
+  transmute(url = glue::glue("https://www.espn.com/nfl/matchup?gameId={id}")) %>%
+  unique()
+
+#get game stats
+games_list <- tibble(
+  html_results = map(game.links$url[1],
+                     ~ {
+                       Sys.sleep(2)
+                       .x %>%
+                         read_html()
+                     }),
+  summary_url = game.links$url[1]
+)
+################################################################################
+#create dataframe of game data
+#this may seem like a lot of code, but to ensure the table scrapes to read as a
+#team,game level data in rows, you need to scrape each element individually
+game.data <- tibble(summary_url = games_list$summary_url, 
+                    home = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".home .short-name") %>%
+                      html_text()),
+                    away = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".away .short-name") %>%
+                      html_text()),
+                    line = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".odds-lines-plus-logo li:nth-child(1)") %>%
+                      html_text()),
+                    over = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes("#gamepackage-game-information li+ li") %>%
+                      html_text()),
+                    home_score =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes("#gamepackage-matchup-wrap .icon-font-before") %>%
+                      html_text()),
+                    away_score = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes("#gamepackage-matchup-wrap .icon-font-after") %>%
+                      html_text()),
+                    home_first_downs =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes("#teamstats-wrap .highlight:nth-child(1) td~ td+ td") %>%
+                      html_text()),
+                    away_first_downs = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes("#teamstats-wrap .highlight:nth-child(1) td:nth-child(2)") %>%
+                      html_text()),
+                    home_plays =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(7) td~ td+ td") %>%
+                      html_text()),
+                    away_plays = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(7) td:nth-child(2)") %>%
+                      html_text()),
+                    home_yards =  map(
+                      games_list$html_results, 
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(8) td~ td+ td") %>%
+                      html_text()),
+                    away_yards = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(8) td:nth-child(2)") %>%
+                      html_text()),
+                    home_drives =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(9) td~ td+ td") %>%
+                      html_text()),
+                    away_drives = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(9) td:nth-child(2)") %>%
+                      html_text()),
+                    home_yds_play =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(10) td~ td+ td") %>%
+                      html_text()),
+                    away_yds_play = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(10) td:nth-child(2)") %>%
+                      html_text()),
+                    home_pass_yds =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(11) td~ td+ td") %>%
+                      html_text()),
+                    away_pass_yds = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(11) td:nth-child(2)") %>%
+                      html_text()),
+                    home_pass_att = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(12) td~ td+ td") %>%
+                      html_text()),
+                    away_pass_att = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(12) td:nth-child(2)") %>%
+                      html_text()),
+                    home_int =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(14) td~ td+ td") %>%
+                      html_text()),
+                    away_int = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(14) td:nth-child(2)") %>%
+                      html_text()),
+                    home_sack =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(15) td~ td+ td") %>%
+                      html_text()),
+                    away_sack = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(15) td:nth-child(2)") %>%
+                      html_text()),
+                    home_rush_yds =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(16) td~ td+ td") %>%
+                      html_text()),
+                    away_rush_yds = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(16) td:nth-child(2)") %>%
+                      html_text()),
+                    home_red_zone =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(19) td~ td+ td") %>%
+                      html_text()),
+                    away_red_zone = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(19) td:nth-child(2)") %>%
+                      html_text()),
+                    home_pen =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(20) td~ td+ td") %>%
+                      html_text()),
+                    away_pen = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(20) td:nth-child(2)") %>%
+                      html_text()), 
+                    home_fumble =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(22) td~ td+ td") %>%
+                      html_text()),
+                    away_fumble = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".indent:nth-child(22) td:nth-child(2)") %>%
+                      html_text()), 
+                    home_def_td =  map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(24) td~ td+ td") %>%
+                      html_text()),
+                    away_def_td = map(
+                      games_list$html_results,
+                      ~ .x %>%
+                      html_nodes(".highlight:nth-child(24) td:nth-child(2)") %>%
+                      html_text()))
